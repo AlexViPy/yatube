0237f83abdf6d106493bc735fb94bd70baa89c4d
Alexandr <a2v@MacBook-Air.local>
2020-11-25 00:43:14 +0300

    changed_25.11.20

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 from .models import Post
 
@@ -8,3 +8,7 @@ def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
     return render(request, "index.html", {"posts": latest})
 
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
